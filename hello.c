b6372924cf8c88c2d23badeffb7593983a7710a4
24955a0302-iare <24955a0302@iare.ac.in>
2026-05-28 13:12:44 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    printf("hello world\n");
+    int a;
+    int b;  
+    printf("enter a \n");
+    scanf("%d",&a);
+    printf("enter b \n");
+    scanf("%d",&b);
+    
+       printf("sum of a&b is : %d\n",a+b);
+       
+    return 0;
+}
